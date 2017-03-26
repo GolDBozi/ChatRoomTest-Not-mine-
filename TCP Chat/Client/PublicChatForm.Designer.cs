63bccd2f7219c1620b11948579f96173a3273e59
GolDBozi <xxxdjorkaeffxxx@gmail.com>
2017-03-26 15:11:51 +0300

    Add files via upload

---
+++ TCP Chat/Client/PublicChatForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Client
+{
+    partial class PublicChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PublicChatForm));
+            this.userList = new System.Windows.Forms.ListBox();
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.privateChat = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtReceive = new System.Windows.Forms.RichTextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // userList
+            // 
+            this.userList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.userList.ContextMenuStrip = this.menu;
+            this.userList.FormattingEnabled = true;
+            this.userList.Location = new System.Drawing.Point(395, 12);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(129, 342);
+            this.userList.TabIndex = 0;
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.privateChat});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(139, 26);
+            // 
+            // privateChat
+            // 
+            this.privateChat.Image = global::Client.Properties.Resources.Chat_128;
+            this.privateChat.Name = "privateChat";
+            this.privateChat.Size = new System.Drawing.Size(138, 22);
+            this.privateChat.Text = "Private Chat";
+            this.privateChat.Click += new System.EventHandler(this.privateChat_Click);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReceive.BackColor = System.Drawing.Color.White;
+            this.txtReceive.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReceive.HideSelection = false;
+            this.txtReceive.Location = new System.Drawing.Point(6, 12);
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ReadOnly = true;
+            this.txtReceive.Size = new System.Drawing.Size(383, 293);
+            this.txtReceive.TabIndex = 1;
+            this.txtReceive.Text = "";
+            this.txtReceive.TextChanged += new System.EventHandler(this.txtReceive_TextChanged);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(6, 311);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(286, 44);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(298, 311);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(91, 44);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "BackColor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ForeColor:Typing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "ForeColor:Chat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // PublicChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 390);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.userList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PublicChatForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCP Chat - Client";
+            this.Load += new System.EventHandler(this.PublicChatForm_Load);
+            this.menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtReceive;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem privateChat;
+        private System.Windows.Forms.ListBox userList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ TCP Chat/Client/PublicChatForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class PublicChatForm : Form
+    {
+        private Random rnd = new Random();
+        System.Media.SoundPlayer s = new System.Media.SoundPlayer(Properties.Resources._1);
+        
+        public PublicChatForm()
+        {
+            pChat = new PrivateChatForm(this);
+            InitializeComponent();
+        }
+        
+        public readonly LoginForm formLogin = new LoginForm();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            formLogin.Client.Received += _client_Received;
+            formLogin.Client.Disconnected += Client_Disconnected;
+            Text = "TCP Chat - " + formLogin.txtIP.Text + " - (Connected as: " + formLogin.txtNickname.Text + ")";
+            formLogin.ShowDialog();
+        }
+
+        private static void Client_Disconnected(ClientSettings cs)
+        {
+            
+        }
+
+        private readonly PrivateChatForm pChat;
+
+        public void _client_Received(ClientSettings cs, string received)
+        {
+            var cmd = received.Split('|');
+            switch (cmd[0])
+            {
+                case "Users":
+                    this.Invoke(() =>
+                    {
+                        userList.Items.Clear();
+                        for (int i = 1; i < cmd.Length; i++)
+                        {
+                            if (cmd[i] != "Connected" | cmd[i] != "RefreshChat")
+                            {
+                                userList.Items.Add(cmd[i]);
+                            }
+                        }
+                    });
+                    break;
+                case "Message":
+                    this.Invoke(() =>
+                    {
+                        txtReceive.Text += cmd[1] + "\r\n";
+                    });
+                    break;
+                case "RefreshChat":
+                    this.Invoke(() =>
+                    {
+                        txtReceive.Text = cmd[1];
+                    });
+                    break;
+                case "Chat":
+                    this.Invoke(() =>
+                    {
+                        pChat.Text = pChat.Text.Replace("user", formLogin.txtNickname.Text);
+                        pChat.Show();
+                    });
+                    break;
+                case "pMessage":
+                    this.Invoke(() =>
+                    {
+                        pChat.txtReceive.Text += "Server says: " + cmd[1] + "\r\n";
+                    });
+                    break;
+                case "Disconnect":
+                    Application.Exit();
+                    break;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        { 
+            if (txtInput.Text != string.Empty)
+            {
+                formLogin.Client.Send("Message|" + formLogin.txtNickname.Text + "|" + txtInput.Text);
+                txtReceive.Text += formLogin.txtNickname.Text + " says: " + txtInput.Text + "\r\n";
+                txtInput.Text = string.Empty;
+            }
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSend.PerformClick();
+            }
+        }
+
+        private void txtReceive_TextChanged(object sender, EventArgs e)
+        {
+            txtReceive.SelectionStart = txtReceive.TextLength;
+          
+            s.Play();
+         
+        }
+
+        private void privateChat_Click(object sender, EventArgs e)
+        {
+            formLogin.Client.Send("pChat|" + formLogin.txtNickname.Text);
+        }
+
+        private void PublicChatForm_Load(object sender, EventArgs e)
+        {
+            Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+          
+            BackColor = randomColor;
+              Color randomColor1 = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            txtInput.ForeColor = randomColor1;
+            txtReceive.ForeColor = txtInput.ForeColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+              
+                txtInput.ForeColor= colorDialog1.Color;
+              
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtReceive.ForeColor = colorDialog1.Color;
+            }
+        }
+    }
+}
